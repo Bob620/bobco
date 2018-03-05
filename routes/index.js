98bb916593c9bebb83f899f186dec89796168e36
bob620 <bruder.kraft225@gmail.com>
2018-03-04 21:11:41 -0600

    redirected bobco.moe/waifu

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { bundle: 'index.js' });
+});
+
+router.get('/waifu', (req, res) => {
+	res.redirect('https://waifubot.moe');
 });
 
 module.exports = router;
