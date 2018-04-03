3eb81c6dea58eb25bccd8a8482b50876b6048259
bob620 <bruder.kraft225@gmail.com>
2018-04-03 16:16:57 -0500

    updt(app) cleaned up

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /assets
-app.use(favicon(path.join(__dirname, 'assets/images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
