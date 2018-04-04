a8394e31b289672dcc7a2671cc856de583a0d7a2
bob620 <bruder.kraft225@gmail.com>
2018-04-03 22:18:54 -0500

    updt(things) cleaned up code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
@@ -12,19 +11,22 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /assets
 app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+if (process.env.NODE_ENV !== 'production') {
+	app.use(require('morgan')('dev'));
+}
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/waifu', (req, res) => {
+	res.redirect('https://waifubot.moe');
+});
 
 app.use('/', index);
-app.use('/waifu', (req, res) => {
-  res.redirect('https://waifubot.moe');
-});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', {bundle: 'index.js'});
+	res.render('index', {bundle: 'index.js', url: ''});
 });
 
 module.exports = router;
