5cf6de7c235d8b9d8dee8580447a8e45edabc5d6
bob620 <bruder.kraft225@gmail.com>
2018-05-25 01:10:19 -0500

    updt(nav) started work on new bot stuff

--- src/index/index.jsx
+++ src/index/index.jsx
@@ -8,6 +8,20 @@ class Home extends Component {
 		return (
 			<div className="App">
 				<Header title="BobCo"/>
+				<div id="App-nav-profile">
+					<div className="nav-profile-image">
+						<img src="./assets/images/waifu.jpg" />
+					</div>
+					<div className="nav-profile-logout">
+						<i className="fas fa-sign-out-alt" />
+					</div>
+					<div className="nav-profile-name">
+						<p>Bob620#5555</p>
+					</div>
+				</div>
+				<div id="App-side-nav">
+
+				</div>
 			</div>
 		);
 	}
